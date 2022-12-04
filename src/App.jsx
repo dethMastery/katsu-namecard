b6da0c39587b400a8cb56942bbb8865bac7c223e
dethMastery <me@suphakit.net>
2022-12-04 21:26:33 +0700

    updated: finalized project

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,33 @@ function App() {
           <img src="https://avatars.githubusercontent.com/u/25049577?v=4" alt="profile" />
         </div>
         <div className="cardContent">
-          <h3>
-            Suphakit Pinyoworapot
-          </h3>
+          <div className="mainComponent">
+            <h3 className="headText">
+              Suphakit Pinyoworapot
+            </h3>
+            <p className="subText">
+              2nd Year Bachelor's degree<br />
+              Computer Engineering<br />
+              @ University of Phayao<br />
+            </p>
+          </div>
+          <div className="contactIcon">
+            <a href="https://fb.com/detzz.in.th" target="_blank" className="facebook">
+              <i class="fa fa-facebook"></i>
+            </a>
+            &nbsp;&nbsp;
+            <a href="https://twitter.com/georgeKdeterk" target="_blank" className="twitter">
+              <i class="fa fa-twitter"></i>
+            </a>
+            &nbsp;&nbsp;
+            <a href="https://github.com/dethMastery" target="_blank" className="github">
+              <i class="fa fa-github"></i>
+            </a>
+          </div>
         </div>
+        <a href="https://suphakit.net/Donation" target="_blank" className="cardFooter">
+          Donation :D
+        </a>
       </div>
     </div>
   )
